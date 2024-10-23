ffc88081061ea69316be6982f6376c0e4374e315
pranil dasika <pranil.dasika@getaugur.ai>
2024-10-23 00:06:10 -0700

    Brew formula update for mantis version v0.1.2

--- mantis.rb
+++ mantis.rb
@@ -5,20 +5,20 @@
 class Mantis < Formula
   desc ""
   homepage "https://getaugur.ai"
-  version "0.1.1"
+  version "0.1.2"
   depends_on :macos
 
   on_intel do
-    url "https://mantis-public-releases.s3.us-east-2.amazonaws.com/mantis/v0.1.1/mantis_0.1.1_darwin_amd64.tar.gz"
-    sha256 "af07cf1b207464e89a120eec9765d7dbc277599e75901d7f109630298164f4f7"
+    url "https://mantis-public-releases.s3.us-east-2.amazonaws.com/mantis/v0.1.2/mantis_0.1.2_darwin_amd64.tar.gz"
+    sha256 "9ceaf77ab6241055166805c8166a9faf3f5df7bf73978921dcb31f86a7be32b8"
 
     def install
       bin.install "mantis"
     end
   end
   on_arm do
-    url "https://mantis-public-releases.s3.us-east-2.amazonaws.com/mantis/v0.1.1/mantis_0.1.1_darwin_arm64.tar.gz"
-    sha256 "3b0dc88cc99a70d8f4bb6dd102d56650cde1c2e23f4d48ae85512f224560c4e1"
+    url "https://mantis-public-releases.s3.us-east-2.amazonaws.com/mantis/v0.1.2/mantis_0.1.2_darwin_arm64.tar.gz"
+    sha256 "1c5ccedc870ef35ef1059728207d0f1597aff06d922635c906653199ebe36c22"
 
     def install
       bin.install "mantis"
